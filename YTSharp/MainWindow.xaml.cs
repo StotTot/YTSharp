c5bb49d6866274ae3c870ccb3bbce17b42183fdd
Aristotle <a.debride.2016@gmail.com>
2023-05-11 20:35:35 -0400

    Dependecy download
    Download button for dependencies for first time use.

--- YTSharp/MainWindow.xaml.cs
+++ YTSharp/MainWindow.xaml.cs
@@ -3,6 +3,10 @@ using System;
 using System.Windows;
 using System.Windows.Controls;
 using YoutubeDLSharp;
+using System.Net;
+using System.IO.Compression;
+using System.IO;
+using System.Windows.Shapes;
 
 namespace YTSharp
 {
@@ -18,27 +22,59 @@ namespace YTSharp
             ytdl = new YoutubeDL();
             ytdl.YoutubeDLPath = "C:\\Users\\Stot_\\Downloads\\yt-dlp.exe";
             ytdl.FFmpegPath = "C:\\Users\\Stot_\\Downloads\\ffmpeg.exe";
-            ytdl.OutputFolder = "C:\\Users\\Stot_\\Downloads\\YT_Downloads\\";
+            ytdl.OutputFolder = "Videos\\YT_Downloads\\";
             InitializeComponent();
         }
-
-        /* TODO: Ability to dowload external binaries.
-         * 
-        async private void Init_Dlp_FfMpeg()
-        {
-            try
-            {
-                this.ytdl.YoutubeDLPath = await YoutubeDLSharp.Utils.DownloadYtDlp();
-                this.ytdl.FFmpegPath = await YoutubeDLSharp.Utils.DownloadFFmpeg();
-            }
-        }
-        */
 
         async private void DownloadButton_Click(object sender, RoutedEventArgs e)
         {
             var progress = new Progress<DownloadProgress>(p => ProgBar.Value = p.Progress);
             var cts = new CancellationTokenSource();
             await ytdl.RunVideoDownload(UrlBox.Text, progress: progress, ct: cts.Token);
+        }
+
+        private void DependButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (WebClient wc = new WebClient())
+            {
+                Console.WriteLine("in the body");
+                wc.DownloadProgressChanged += wc_DownloadProgressChanged;
+                wc.DownloadFile(
+                    // Param1 = Link of file
+                    new System.Uri("https://github.com/yt-dlp/yt-dlp/releases/download/2023.03.04/yt-dlp.exe"),
+                    // Param2 = Path to save
+                    "bin\\yt-dlp.exe"
+                );
+                wc.DownloadFile(
+                    // Param1 = Link of file
+                    new System.Uri("https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-gpl.zip"),
+                    // Param2 = Path to save
+                    "ffmpeg-master-latest-win64-gpl.zip"
+                );
+            }
+
+            string zipPath = "ffmpeg-master-latest-win64-gpl.zip";
+            string extractPath = "ffmpeg";
+
+            ZipFile.ExtractToDirectory(zipPath, extractPath);
+
+            File.Move("ffmpeg\\ffmpeg-master-latest-win64-gpl\\bin\\ffmpeg.exe", "bin\\ffmpeg.exe");
+
+            Directory.Delete("ffmpeg", true);
+
+            File.Delete(zipPath);
+
+            //var ffmpegZip = ZipFile.OpenRead("ffmpeg-master-latest-win64-gpl.zip");
+            //var entry = ffmpegZip.GetEntry("ffmpeg-master-latest-win64-gpl\\bin\\ffmpeg.exe");
+            //entry.ExtractToFile("ffmpeg.exe", true);
+
+            ytdl.YoutubeDLPath = "yt-dlp.exe";
+            ytdl.FFmpegPath = "ffmpeg.exe";
+        }
+
+        void wc_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            ProgBar.Value = e.ProgressPercentage;
         }
     }
 }
