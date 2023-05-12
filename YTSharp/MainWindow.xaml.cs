673bb0342c39b2b42fcbe6a56c1e26f2c7ca7faf
Aristotle <a.debride.2016@gmail.com>
2023-05-12 15:22:51 -0400

    Better prog bar

--- YTSharp/MainWindow.xaml.cs
+++ YTSharp/MainWindow.xaml.cs
@@ -7,6 +7,10 @@ using System.Net;
 using System.IO.Compression;
 using System.IO;
 using System.Windows.Shapes;
+using System.Threading.Tasks;
+using YoutubeDLSharp.Metadata;
+using System.Security.Policy;
+using System.Diagnostics;
 
 namespace YTSharp
 {
@@ -17,28 +21,42 @@ namespace YTSharp
     {
         public YoutubeDL ytdl { get; }
 
+        private IProgress<DownloadProgress> prog;
+
         public MainWindow()
         {
             ytdl = new YoutubeDL();
             ytdl.YoutubeDLPath = "C:\\Users\\Stot_\\Downloads\\yt-dlp.exe";
             ytdl.FFmpegPath = "C:\\Users\\Stot_\\Downloads\\ffmpeg.exe";
-            ytdl.OutputFolder = "Videos\\YT_Downloads\\";
+            ytdl.OutputFolder = "YT_Downloads\\";
+            prog = new Progress<DownloadProgress>((p) => showProg(p));
             InitializeComponent();
         }
 
+        private void showProg(DownloadProgress p)
+        {
+            ProgBar.Value = p.Progress * 100;
+            TextProg.Text = $"speed: {p.DownloadSpeed} | ETA: {p.ETA}";
+        }
         async private void DownloadButton_Click(object sender, RoutedEventArgs e)
         {
-            var progress = new Progress<DownloadProgress>(p => ProgBar.Value = p.Progress);
             var cts = new CancellationTokenSource();
-            await ytdl.RunVideoDownload(UrlBox.Text, progress: progress, ct: cts.Token);
+            //RunResult<VideoData> mtdta = await ytdl.RunVideoDataFetch(UrlBox.Text);
+            //if (mtdta.Success)
+            //{
+            //    Console.WriteLine(mtdta.Data);
+            //}
+            var res = await ytdl.RunVideoDownload(UrlBox.Text, progress: prog, ct: cts.Token);
+            if (res.Success)
+            {
+                MessageBox.Show("Successfully downloaded " + UrlBox.Text, "YTSharp");
+            }
         }
-
         private void DependButton_Click(object sender, RoutedEventArgs e)
         {
             using (WebClient wc = new WebClient())
             {
                 Console.WriteLine("in the body");
-                wc.DownloadProgressChanged += wc_DownloadProgressChanged;
                 wc.DownloadFile(
                     // Param1 = Link of file
                     new System.Uri("https://github.com/yt-dlp/yt-dlp/releases/download/2023.03.04/yt-dlp.exe"),
@@ -70,11 +88,34 @@ namespace YTSharp
 
             ytdl.YoutubeDLPath = "yt-dlp.exe";
             ytdl.FFmpegPath = "ffmpeg.exe";
-        }
 
-        void wc_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+            MessageBox.Show("Successfully downloaded dependencies", "YTSharp");
+        }
+        // TODO: Still need a quality selector.
+        //private async void FindButton_Click(object sender, RoutedEventArgs e)
+        //{
+        //    var mtdta = await ytdl.RunVideoDataFetch(UrlBox.Text);
+        //    VideoData video = mtdta.Data;
+        //    FormatData[] formats = video.Formats;
+        //    for (int i  = 0; i < formats.Length; i++)
+        //    {
+        //        Console.WriteLine(formats[i].Quality.ToString);
+        //    }
+        //}
+
+        async private void DownloadAudioButton_Click(object sender, RoutedEventArgs e)
         {
-            ProgBar.Value = e.ProgressPercentage;
+            var cts = new CancellationTokenSource();
+            //RunResult<VideoData> mtdta = await ytdl.RunVideoDataFetch(UrlBox.Text);
+            //if (mtdta.Success)
+            //{
+            //    Console.WriteLine(mtdta.Data);
+            //}
+            var res = await ytdl.RunAudioDownload(UrlBox.Text, progress: prog, ct: cts.Token);
+            if (res.Success)
+            {
+                MessageBox.Show("Successfully downloaded " + UrlBox.Text, "YTSharp");
+            }
         }
     }
 }
